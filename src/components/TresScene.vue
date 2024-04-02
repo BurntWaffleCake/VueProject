0c4827ce797ff0efe045ba03c86ad6218fe807e9
BurntWaffleCake <neondenetro@gmail.com>
2024-04-01 22:30:28 -0500

    about me background

--- src/components/TresScene.vue
+++ src/components/TresScene.vue
@@ -1,17 +1,60 @@
 <script lang="ts" setup>
 import { TresCanvas, useRenderLoop } from "@tresjs/core";
+import { onMounted } from "vue";
 import { ref } from "vue";
 
+function interpolateHexColors(color1: string, color2: string, k: number) {
+  function hexToRgb(hex) {
+    return hex.match(/[A-Za-z0-9]{2}/g).map(function (v) {
+      return parseInt(v, 16);
+    });
+  }
+
+  function rgbToHex(rgb) {
+    return (
+      "#" +
+      rgb
+        .map(function (v) {
+          return v.toString(16).padStart(2, "0");
+        })
+        .join("")
+    );
+  }
+
+  const rgb1 = hexToRgb(color1);
+  const rgb2 = hexToRgb(color2);
+
+  const resultRgb = rgb1.map((channel, index) => Math.round(channel + (rgb2[index] - channel) * k));
+
+  return rgbToHex(resultRgb);
+}
+
+const props = defineProps({
+  backgroundColor: String,
+});
+
+console.log(props);
+
+const fromColor = "#000000";
+const toColor = "#1f1f1f";
+
+const color = ref(interpolateHexColors(fromColor, toColor, 0));
 const torus1 = ref();
 const torus2 = ref();
 const torus3 = ref();
+const torus4 = ref();
 
 const { onLoop } = useRenderLoop();
 
 const target = document.getElementById("app");
 
-target?.addEventListener("scroll", () => {
-  const scrollRatio = (target.scrollTop + target.scrollHeight / 2) / target.scrollHeight;
+function tick() {
+  var h = target,
+    b = document.body,
+    st = "scrollTop",
+    sh = "scrollHeight";
+
+  var scrollRatio = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight);
 
   torus1.value.rotation.y = 10 * scrollRatio;
   torus1.value.rotation.x = 5 * scrollRatio;
@@ -20,26 +63,38 @@ target?.addEventListener("scroll", () => {
   torus2.value.rotation.x = 7 * scrollRatio + 0.5;
 
   torus3.value.rotation.y = 3 * scrollRatio + 0.23;
-});
+
+  torus4.value.rotation.x = 1.5 * scrollRatio + 0.945;
+
+  color.value = interpolateHexColors(fromColor, toColor, scrollRatio);
+}
+target?.addEventListener("scroll", tick);
+
+onMounted(() => {});
 </script>
 
 <template>
-  <TresCanvas clear-color="#82DBC5" shadows window-size>
+  <TresCanvas :clear-color="color" shadows window-size>
     <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
 
     <TresMesh ref="torus1">
       <TresTorusGeometry :args="[0.7, 0.3, 16, 32]" />
-      <TresMeshToonMaterial color="#82DBC5" />
+      <TresMeshToonMaterial color="#82DBC5" :wireframe="true" />
     </TresMesh>
 
     <TresMesh ref="torus2">
       <TresTorusGeometry :args="[1.5, 0.35, 16, 32]" />
-      <TresMeshToonMaterial color="#82DBC5" />
+      <TresMeshToonMaterial color="#156064" :wireframe="true" />
     </TresMesh>
 
     <TresMesh ref="torus3">
       <TresTorusGeometry :args="[3, 0.4, 16, 32]" />
-      <TresMeshToonMaterial color="#82DBC5" />
+      <TresMeshToonMaterial color="#F8E16C" :wireframe="true" />
+    </TresMesh>
+
+    <TresMesh ref="torus4">
+      <TresTorusGeometry :args="[5, 0.5, 16, 32]" />
+      <TresMeshToonMaterial color="#FB8F67" :wireframe="true" />
     </TresMesh>
 
     <TresDirectionalLight :position="[0, 2, 4]" :intensity="1.2" cast-shadow />
--- src/components/Views/AboutMeView.vue
+++ src/components/Views/AboutMeView.vue
@@ -1,21 +1,59 @@
 <script setup>
 import { ref } from "vue";
 import TresScene from "../TresScene.vue";
-const isVisible = ref(true);
 </script>
 
 <template>
   <div class="d-flex flex-column flex-grow-1 h-auto">
     <TresScene style="z-index: -10" />
-    <div
-      :class="isVisible ? 'd-flex' : 'd-none'"
-      class="px-0 container-fluid w-100 vh-100 d-flex flex-column justify-content-center align-items-center"
-      style="scroll-snap-align: start"
-    >
-      <div class="display-1 text-light">Soohan Cho<br /></div>
-      <div class="display-5 text-light">So who is he?</div>
+    <div class="px-0 container-fluid w-100 vh-100 d-flex flex-column justify-content-center" style="scroll-snap-align: start">
+      <div class="w-100 d-flex flex-column align-items-center bg-body py-4">
+        <div class="display-1">Soohan Cho<br /></div>
+        <div class="display-5 text-nowrap scroll-infinite">
+          So who is he? &emsp; So who is he? &emsp; So who is he? &emsp; So who is he? &emsp; So who is he? &emsp; So who is he? &emsp; So who is he?
+          &emsp; So who is he? &emsp; So who is he? &emsp; So who is he? &emsp; So who is he? &emsp; So who is he? &emsp; So who is he? &emsp;
+        </div>
+      </div>
     </div>
 
-    <div class="px-0 container-fluid w-100 vh-100" style="scroll-snap-align: start"></div>
+    <div class="px-0 container-fluid w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div class="h3 text-center bg-body p-5 rounded-5">
+        <h1>Skills</h1>
+        <hr style="height: 3px" />
+        Computer Scientist<br />
+        Graphic Designer<br />
+        Artist<br />
+        Scientist<br />
+        Game Developer<br />
+        Programmer<br />
+        Full Stack Developer<br />
+        Web Application Designer<br />
+        <hr style="height: 3px" />
+      </div>
+    </div>
+
+    <div class="px-0 container-fluid w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div class="text-shadow-fancy h1 text-light text-center">My name is Soohan Cho. I am an</div>
+    </div>
   </div>
 </template>
+
+<style>
+.text-shadow-fancy {
+  text-shadow: 0px 0px 0.5rem rgb(0, 0, 0);
+}
+
+.scroll-infinite {
+  animation: scroll 10s infinite linear;
+}
+
+@keyframes scroll {
+  0% {
+    translate: -25%;
+  }
+
+  100% {
+    translate: 25%;
+  }
+}
+</style>
